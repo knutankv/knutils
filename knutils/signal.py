03c6b5380f5b2d73f8815f040b280a5f3bc07d47
Knut Andreas Kvåle <knut.a.kvale@gmail.com>
2024-06-24 13:17:01 +0200

    Update signal.py

--- knutils/signal.py
+++ knutils/signal.py
@@ -1,6 +1,15 @@
 import numpy as np
 from scipy.signal import csd
 
+from scipy.signal import butter, sosfilt, sosfiltfilt, sosfreqz
+def butter_construct(cuts, fs, btype='band', order=5):
+        nyq = 0.5 * fs
+        cuts = [cut/nyq for cut in cuts]
+        sos = butter(order, cuts, analog=False, btype=btype, output='sos')
+
+        return sos
+
+
 def xwelch(x, **kwargs):
     f, __ = csd(x[:,0], x[:,0], **kwargs)
     cpsd = np.zeros([x.shape[1], x.shape[1], len(f)]).astype('complex')
